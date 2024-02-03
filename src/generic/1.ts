dbc5a1d191e7a131186816f8035b5434a1e4da2e
Sergey Kabanets <skabanets97@gmail.com>
2024-02-03 03:01:21 +0200

    fix generic task-1

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,9 +3,9 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T>(): Promise<Array<T>> {
-  return new Promise((resolve) => {
-    resolve(["Text", 50] as Array<T>);
+function getPromise(): Promise<(string | number)[]> {
+  return new Promise<(string | number)[]>((resolve) => {
+    resolve(["Text", 50]);
   });
 }
 
